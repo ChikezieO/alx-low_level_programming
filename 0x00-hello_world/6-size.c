ef22a06df57ff74623a50c41b90418ffb6f61d33
ChikezieO <chtokorocha@gmail.com>
2023-06-15 20:16:34 -0700

    Answer task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the various types of computer sizes
+ * Return:0 (Success)
+ */
+int main(void)
+{
+int num;
+printf("size of char:%zu bytes\n", size of(char));
+printf("size of int:%zu bytes\n", size of(int));
+printf("size of long int:%zu bytes\n", size of(long int));
+printf("size of long long int:%zu bytes\n", size of(long long int));
+printf("size of float:%zu bytes\n", size of(float));
+return (0);
+}
