80b5bd791a6e0cf09405d253e4a293adce3eff59
ChikezieO <chtokorocha@gmail.com>
2023-06-19 21:35:26 -0700

    Answer task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int num;
 
-	printf("size of char: %c byte(s)\n", sizeof(char));
+	printf("size of char: %d byte(s)\n", sizeof(char));
 	printf("size of int: %d byte(s)\n", sizeof(int));
 	printf("size of long int: %u byte(s)\n", sizeof(long long int));
 	printf("size of long long int: %u byte(s)\n", sizeof(long long int));
