1d70f580eeed61140a678aa98b1d7438e8afb335
ChikezieO <chtokorocha@gmail.com>
2023-06-20 21:53:53 -0700

    Answer task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make lower case alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet()
+{
+	char  ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	{
+		_putchar('\n');
+	}
+}
