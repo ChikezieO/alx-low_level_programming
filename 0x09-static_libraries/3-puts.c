fba2e62064d705e99a11c4c31ea05215241a8538
ChikezieO <chtokorocha@gmail.com>
2023-07-11 12:13:49 -0700

    Answer task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ *
+ * @s: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *s)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
